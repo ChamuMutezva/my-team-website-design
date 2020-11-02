acbd919d2589178b58ab20aae2ce6297c20d4f8e
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-11-02 06:44:31 +0200

    about page - business card draft

---
+++ directors.js
@@ -0,0 +1,40 @@
+const directorsList = [
+    {
+        name: "Nikita Marks",
+        position: "Founder & CEO",
+        profileUrl: "./images/avatar-nikita.jpg",
+        remarks: `It always amazes me how much talent there is in every corner of the globe.`
+    },
+    {
+        name: " Cristian Duncan",
+        position: "Co-founder & COO",
+        profileUrl: "./images/avatar-christian.jpg",
+        remarks: `Distributed teams required unique processes. You need to approach work in a new way.`
+    },
+    {
+        name: "Cruz Hamer",
+        position: " Co-founder & CTO",
+        profileUrl: "./images/avatar-cruz.jpg",
+        remarks: `Technology is at the forefront of enabling distributed teams. That's where we come in.`
+    },
+    {
+        name: "Drake Heaton",
+        position: "Business Development Lead",
+        profileUrl: "./images/avatar-drake.jpg",
+        remarks: `Hiring similar people from similar backgrounds is a surefire way to stunt innovation.`
+    },
+    {
+        name: "Griffin Wise",
+        position: "Lead Marketing",
+        profileUrl: "./images/avatar-griffin.jpg",
+        remarks: `Unique perspectives shape unique products, which is what you need to survive these days.`
+    },
+    {
+        name: "Aden Allan",
+        position: "Head of Talent",
+        profileUrl: "./images/avatar-aden.jpg",
+        remarks: `Empowered teams create truly amazing products. Set the north star and let them follow it.`
+    },
+    
+]
+ 
