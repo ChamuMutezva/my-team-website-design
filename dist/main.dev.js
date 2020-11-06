4ec387fce901370d0fd117112b5afd8cb434e226
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-11-06 06:21:05 +0200

    feat: toggle the business cards in about page when clicked

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -4,12 +4,28 @@
 var openModal = document.querySelector(".hamburger");
 var closeMenu = document.querySelector(".closemenu");
 var modal = document.querySelector(".modal");
-console.log(openModal);
-console.log(modal);
+var cross = Array.from(document.querySelectorAll(".cross"));
+var allCards = document.querySelector(".allCards"); //clickTarget.closest("li") to find nearest li tag 
+
+console.log(allCards); //console.log(cross)
+
 openModal.addEventListener("click", function () {
   modal.classList.toggle("showModal");
 });
 closeMenu.addEventListener("click", function () {
   console.log("i have been clicked");
   modal.classList.toggle("showModal");
+});
+allCards.addEventListener("click", function (event) {
+  var clickedTarget = event.target;
+  console.log(clickedTarget);
+
+  if (clickedTarget.closest("div").classList.contains("cross")) {
+    var allFigures = Array.from(document.querySelectorAll("figure"));
+    console.log(clickedTarget.closest("div").nextElementSibling);
+    clickedTarget.closest("div").nextElementSibling.classList.toggle("displayCardModal");
+    var figureTarget = document.querySelector("figure");
+    clickedTarget.closest("figure").previousElementSibling.classList.toggle("hideBusinessCard"); // console.log(allFigures)
+    // figureTarget.classList.toggle("hideBusinessCard");
+  }
 });
--- main.js
+++ main.js
@@ -2,13 +2,30 @@
 const openModal = document.querySelector(".hamburger");
 const closeMenu = document.querySelector(".closemenu");
 const modal = document.querySelector(".modal");
-console.log(openModal);
-console.log(modal)
+const cross = Array.from(document.querySelectorAll(".cross"));
+const allCards = document.querySelector(".allCards");
+//clickTarget.closest("li") to find nearest li tag 
+console.log(allCards);
+//console.log(cross)
 openModal.addEventListener("click", () => {
     modal.classList.toggle("showModal")
 })
 closeMenu.addEventListener("click", () => {
     console.log("i have been clicked")
     modal.classList.toggle("showModal")
+})
+allCards.addEventListener("click", (event) => {
+    const clickedTarget = event.target;
+    console.log(clickedTarget)
+    if(clickedTarget.closest("div").classList.contains("cross")){
+        const allFigures = Array.from(document.querySelectorAll("figure"))
+        console.log(clickedTarget.closest("div").nextElementSibling)
+       clickedTarget.closest("div").nextElementSibling.classList.toggle("displayCardModal");
+       const figureTarget = document.querySelector("figure")
+       clickedTarget.closest("figure").previousElementSibling.classList.toggle("hideBusinessCard");
+      // console.log(allFigures)
+      // figureTarget.classList.toggle("hideBusinessCard");
+    }
+
 })
 
